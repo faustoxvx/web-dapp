e0abb32bf49a59597183ff21d8f7fbc119c0a3b9
Fausto Nunes <faustornunes@gmail.com>
2023-07-21 13:46:17 +0100

    Delete career_update working - now destroying career_update_associations

--- app/models/career_update.rb
+++ app/models/career_update.rb
@@ -1,6 +1,6 @@
 class CareerUpdate < ApplicationRecord
   belongs_to :user
-  has_many :career_update_associations
+  has_many :career_update_associations, dependent: :destroy
   has_many :goals, through: :career_update_associations, source: :associable_entity, source_type: "Goal"
   has_many :milestones, through: :career_update_associations, source: :associable_entity, source_type: "Milestone"
 
